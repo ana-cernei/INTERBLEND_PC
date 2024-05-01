13656420609ecae98890a31eaff971d0ff98cb6b
Ana <ana.cernei03@e-uvt.ro>
2024-05-01 13:31:21 +0300

    changed back

--- partials/_checkoutModal.php
+++ partials/_checkoutModal.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="checkoutModal">ENTER Your Details:</h5>
+            <h5 class="modal-title" id="checkoutModal">Enter Your Details:</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
